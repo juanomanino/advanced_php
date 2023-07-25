697d3bfea9c6229bf1ca58332e4ccdcc0bfc8e6d
juanomanino <juannino@colegioguanenta.edu.co>
2023-07-25 07:51:55 -0500

    Todos los Ejemplos.Falta ReadMe

--- 7_cookies.php
+++ 7_cookies.php
@@ -1,3 +1,19 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+// set the expiration date to one hour ago
+setcookie("user", "", time() - 3600);
+
+
+setcookie("test_cookie", "test", time() + 3600, '/');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +23,30 @@
 </head>
 <body>    
     <?php
+
+    // echo "<hr><h2>Crear-Recoger Cookie</h2>";
+
+    // if(!isset($_COOKIE[$cookie_name])) {
+    //     echo "Cookie named '" . $cookie_name . "' is not set!";
+    //     } else {
+    //     echo "Cookie '" . $cookie_name . "' is set!<br>";
+    //     echo "Value is: " . $_COOKIE[$cookie_name];
+    //     }
+
+    //echo "Cookie 'user' is deleted.";
+
+    echo "<br>";
+
+    if(count($_COOKIE) > 0) {
+        echo "Cookies are enabled.";
+      } else {
+        echo "Cookies are disabled.";
+      }
+
     
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
     ?>
     
     
---
+++ 8_sesiones/demo_session2.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+
+
+// to change a session variable, just overwrite it
+$_SESSION["favcolor"] = "yellow";
+echo   "<br>";
+print_r($_SESSION);
+?>
+
+
+</body>
+</html>
