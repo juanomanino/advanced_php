85a104b2e9107904486eb82f28ec409a12d7c1b6
Kevinsolanoxd <kevinsolano@colegioguanenta.edu.co>
2023-07-25 07:01:20 -0500

    se sube desde el 8 hasta el 13 funcionales exepto el 13

--- 13_exceptions.php
+++ 13_exceptions.php
@@ -3,12 +3,125 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fecha y Hora en PHP</title>
+    <title>Excepciones de PHP</title>
 </head>
 <body>
-    <php?
-    
+    <h1>Lanzar una excepción</h1>
+    <p>La throwinstrucción permite que una función o método definido por el usuario genere una excepción. Cuando se lanza una excepción, el código que le sigue no se ejecutará.
+
+    Si no se detecta una excepción, se producirá un error fatal con un mensaje de "Excepción no detectada".
+
+    Intentemos lanzar una excepción sin atraparla:</p>
+
+    <?php
+    function divide($dividend, $divisor) {
+      if($divisor == 0) {
+        throw new Exception("Division by zero");
+      }
+      return $dividend / $divisor;
+    }
+
+    echo divide(5, 0);
     ?>
-    
+
+    <hr>
+
+    <h1>La sentencia try... catch</h1>
+    <p>Para evitar el error del ejemplo anterior, podemos usar la try...catchdeclaración para detectar excepciones y continuar el proceso.</p>
+
+    <?php
+    try {
+        code that can throw exceptions
+      } catch(Exception $e) {
+        code that runs when an exception is caught
+      }
+    ?>
+
+    <?php
+    function divide2($dividend, $divisor) {
+      if($divisor == 0) {
+        throw new Exception("Division by zero");
+      }
+      return $dividend / $divisor;
+    }
+
+    try {
+      echo divide2(5, 0);
+    } catch(Exception $e) {
+      echo "Unable to divide.";
+    }
+    ?>
+    <hr>
+    <h1>La declaración intentar... atrapar... finalmente</h1>
+    <p>La try...catch...finallyinstrucción se puede utilizar para detectar excepciones. El código en el finallybloque siempre se ejecutará independientemente de si se detectó una excepción. Si finallyestá presente, el catchbloque es opcional.</p>
+    <?php
+    try {
+        code that can throw exceptions
+      } catch(Exception $e) {
+        code that runs when an exception is caught
+      } finally {
+        code that always runs regardless of whether an exception was caught
+      }
+    ?>
+
+    <?php
+    function divide3($dividend, $divisor) {
+      if($divisor == 0) {
+        throw new Exception("Division by zero");
+      }
+      return $dividend / $divisor;
+    }
+
+    try {
+      echo divide3(5, 0);
+    } finally {
+      echo "Process complete.";
+    }
+    ?>
+
+    <p>Muestra una cadena incluso si no se detectó una excepción:</p>
+
+    <?php
+    function divide($dividend, $divisor) {
+      if($divisor == 0) {
+        throw new Exception("Division by zero");
+      }
+      return $dividend / $divisor;
+    }
+
+    try {
+      echo divide(5, 0);
+    } finally {
+      echo "Process complete.";
+    }
+    ?>
+
+    <hr>
+    <h1>El objeto de excepción</h1>
+    <p>El objeto de excepción contiene información sobre el error o el comportamiento inesperado que encontró la función.</p>
+
+    <?php
+    new Exception(message, code, previous)
+    ?>
+    <?php
+    function divide4($dividend, $divisor) {
+      if($divisor == 0) {
+        throw new Exception("Division by zero", 1);
+      }
+      return $dividend / $divisor;
+    }
+
+    try {
+      echo divide4(5, 0);
+    } catch(Exception $ex) {
+      $code = $ex->getCode();
+      $message = $ex->getMessage();
+      $file = $ex->getFile();
+      $line = $ex->getLine();
+      echo "Exception thrown in $file on line $line: [Code $code]
+      $message";
+    }
+    ?>
+
 </body>
 </html>
--- 9_filters.php
+++ 9_filters.php
@@ -3,13 +3,136 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fecha y Hora en PHP</title>
+    <title>Filtros php</title>
 </head>
 <body>    
-    <php?
-    
-    ?>
-    
-    
+    <h1>La extensión de filtro PHP</h1>
+    <p>Los filtros PHP se utilizan para validar y desinfectar la entrada externa.
+
+    La extensión de filtro PHP tiene muchas de las funciones necesarias para verificar la entrada del usuario y está diseñada para hacer que la validación de datos sea más fácil y rápida.
+
+    La filter_list()función se puede usar para enumerar lo que ofrece la extensión de filtro PHP:</p>
+
+    <table>
+      <tr>
+        <td>Filter Name</td>
+        <td>Filter ID</td>
+      </tr>
+  <?php
+  foreach (filter_list() as $id =>$filter) {
+    echo '<tr><td>' . $filter . '</td><td>' . filter_id($filter) . '</td></tr>';
+  }
+  ?>
+  </table>
+  <hr>
+
+  <h1>
+  Desinfectar una cuerda
+  </h1>
+
+  <p>El siguiente ejemplo usa la filter_var()función para eliminar todas las etiquetas HTML de una cadena:</p>
+
+  <?php
+$str = "<h1>Hello World!</h1>";
+$newstr = filter_var($str, FILTER_SANITIZE_STRING);
+echo $newstr;
+echo"<hr>"
+?>
+
+<h1>Validar un entero</h1>
+
+<p>El siguiente ejemplo usa la filter_var()función para verificar si la variable $int es un número entero. Si $int es un número entero, la salida del siguiente código será: "El número entero es válido". Si $int no es un número entero, la salida será: "El número entero no es válido":
+</p>
+
+<?php
+$int = 100;
+
+if (!filter_var($int, FILTER_VALIDATE_INT) === false) {
+  echo("Integer is valid");
+} else {
+  echo("Integer is not valid");
+}
+?>
+<hr>
+
+<h1>Sugerencia: filter_var() y problema con 0</h1>
+
+<p>En el ejemplo anterior, si $int se estableció en 0, la función anterior devolverá "El número entero no es válido". Para resolver este problema, utilice el siguiente código:</p>
+
+<?php
+$int = 0;
+
+if (filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
+  echo("Integer is valid");
+} else {
+  echo("Integer is not valid");
+}
+?>
+<hr>
+<h1>Validar una dirección IP</h1>
+<p>El siguiente ejemplo usa la filter_var()función para verificar si la variable $ip es una dirección IP válida:</p>
+
+
+<?php
+$ip = "127.0.0.1";
+
+if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
+  echo("$ip is a valid IP address");
+} else {
+  echo("$ip is not a valid IP address");
+}
+?>
+<hr>
+
+<h1>Desinfectar y validar una dirección de correo electrónico</h1>
+
+<p>El siguiente ejemplo usa la filter_var()función para eliminar primero todos los caracteres ilegales de la variable $email, luego verifica si es una dirección de correo electrónico válida:</p>
+
+<?php
+$email = "john.doe@example.com";
+
+// Remove all illegal characters from email
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// Validate e-mail
+if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+  echo("$email is a valid email address");
+} else {
+  echo("$email is not a valid email address");
+}
+?>
+<hr>
+
+<h1>Desinfectar y validar una URL</h1>
+
+<p>El siguiente ejemplo usa la filter_var()función para eliminar primero todos los caracteres ilegales de una URL y luego verificar si $url es una URL válida:</p>
+
+
+<?php
+$url = "https://www.w3schools.com";
+
+// Remove all illegal characters from a url
+$url = filter_var($url, FILTER_SANITIZE_URL);
+
+// Validate url
+if (!filter_var($url, FILTER_VALIDATE_URL) === false) {
+  echo("$url is a valid URL");
+} else {
+  echo("$url is not a valid URL");
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
